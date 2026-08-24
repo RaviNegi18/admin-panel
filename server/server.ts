a6d6c19fc3a9bbb45a5c8da2ccb468c7548c525e
Ravinder singh <negiravi451@gmail.com>
2026-08-25 00:45:48 +0530

    setup redux and backend structure

--- server/server.ts
+++ server/server.ts
@@ -1,10 +1,11 @@
 import express from "express"
-
-
+import userRoutes from "./../server/routes/auth.routes.js"
+import connectDB from "./config/DB.ts";
 const PORT=5000;
 const app=express()
+connectDB()
 
-
+app.use("/api",userRoutes)
 
 app.listen(PORT,()=>{
     console.log(`your app is listening on http://localhost:${PORT}`)
