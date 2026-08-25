e5a48701b2dc18abec24e60a46453c6df83a9ddf
Ravinder singh <negiravi451@gmail.com>
2026-08-25 23:59:01 +0530

    redux setup

--- client/src/redux/authSlice.ts
+++ client/src/redux/authSlice.ts
@@ -1,1 +1,15 @@
 import {createSlice,createAsyncThunk} from "@reduxjs/toolkit"
+import axios from "axios"
+import type { LoginData,RegisterData,AuthState } from "./authTypes"
+
+const API_URI=import.meta.env.VITE_API_URL
+
+
+const registerUser=createAsyncThunk("auth/registerUser",async(userData,thunkAPI)=>{
+    try{
+const response=await axios.post(`${API_URI}/register`,userData)
+return response.data
+    }catch(error){
+return thunkAPI.rejectWithValue(error)
+    }
+})
