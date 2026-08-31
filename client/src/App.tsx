02cd9e5675f0b2bf3b2f0e71246d10dcf7ec1515
Ravinder singh <negiravi451@gmail.com>
2026-08-31 23:47:24 +0530

    completed the dashboard

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter,Routes,Route } from 'react-router-dom'
 import SignIn from "./pages/SignIn.tsx"
 import SignUp from './pages/SignUp.tsx'
+import Dashboard from './pages/Dashboard.tsx'
+import Products from './components/Products.tsx'
 const App = () => {
   return (
     <BrowserRouter>
     <Routes>
       <Route  path='/' element={<SignIn/>}/>
       <Route path='/sign-up' element={<SignUp/>}/>
+      <Route path='/products' element={<Products/>}/>
+      <Route path='/dashboard' element={<Dashboard/>}/>
     </Routes>
     </BrowserRouter>
   )
---
+++ client/src/components/Products.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Product } from '../types/product'
+const Products = () => {
+  const [product,setProducts]
+  return (
+    <div>Products</div>
+  )
+}
+
+export default Products
