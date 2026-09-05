5b5ce64ac59c675577eef4f02200ffe0051d6028
Ravinder singh <negiravi451@gmail.com>
2026-09-05 23:56:30 +0530

    set up the redux store

--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../redux/authSlice";
 import cartReducer from "../features/slice/cartSlice";
 
+//setup store
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     cart: cartReducer,
   },
 });
+//set uptypes
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
