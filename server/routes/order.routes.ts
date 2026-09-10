6088a999fbecd838c89283ee0ae9b9c27b4e9cd2
Ravinder singh <negiravi451@gmail.com>
2026-09-11 00:50:40 +0530

    add order routes for creating new orders

---
+++ server/routes/order.routes.ts
@@ -0,0 +1,7 @@
+import express from "express"
+import { createOrder } from "../controllers/orderController.ts";
+const router = express.Router();
+
+router.post("/orders",createOrder)
+
+export default router
