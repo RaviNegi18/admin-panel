104078a8417992963fc7f514d19602c10ca91ce8
Ravinder singh <negiravi451@gmail.com>
2026-09-14 23:38:47 +0530

    add checkout session route to order routes

--- server/routes/order.routes.ts
+++ server/routes/order.routes.ts
@@ -1,7 +1,8 @@
 import express from "express"
 import { createOrder } from "../controllers/orderController.ts";
+import { createCheckoutSession } from "../controllers/paymentController.ts";
 const router = express.Router();
 
 router.post("/orders",createOrder)
-
+router.post("/:orderId/checkout", createCheckoutSession);
 export default router
