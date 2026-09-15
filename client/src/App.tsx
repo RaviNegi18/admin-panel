9dc6c5711bd3448952d7785dc0a49d0c48de3c0a
Ravinder singh <negiravi451@gmail.com>
2026-09-15 22:52:53 +0530

    add payment routes for checkout and success pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,6 +5,7 @@ import Dashboard from './pages/Dashboard.tsx'
 import Products from './components/Products.tsx'
 import ProductDetails from "./components/ProductDetails.tsx"
 import Cart from './components/CartPage.tsx'
+import Payment, { PaymentSuccess } from './components/Payment.tsx'
 
 const App = () => {
   return (
@@ -15,6 +16,8 @@ const App = () => {
         <Route path='/details/:id' element={<ProductDetails />} />
         <Route path='/products' element={<Products />} />
         <Route path="/cart" element={<Cart />} />
+        <Route path="/payment" element={<Payment />} />
+        <Route path="/payment/success" element={<PaymentSuccess />} />
         <Route path='/dashboard' element={<Dashboard />} />
       </Routes>
     </BrowserRouter>
